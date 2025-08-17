0c621ee3c433fa250d2699ad42243f3ef29cc2b6
srinivassaitangudu <157262280+srinivassaitangudu@users.noreply.github.com>
2025-08-16 21:47:22 -0400

    Added Settings, About pages.

--- frontend/main.py
+++ frontend/main.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import os, tempfile
 
-import time
 import backend.resume_parser as barepa
+import utils.theme as theme_utils
+
+IS_DISABLED = True
 
 
 def save_uploaded_to_temp(uploaded_file) -> str:
@@ -12,33 +14,41 @@ def save_uploaded_to_temp(uploaded_file) -> str:
         tmp.write(uploaded_file.getbuffer())
         return tmp.name
 
+def resume_parser():
+    with st.spinner("Parsing resume..."):
+        resume_parser = barepa.ResumeParser()
+        path = save_uploaded_to_temp(uploaded_file)
+        file_content = resume_parser.parse_resume(path)
+        st.write(file_content)
+    st.success("Questions generated successfully!")
+    
+    
+# Streamlit App
+st.set_page_config(page_title="Interview Agent", page_icon="utils/images/icon.png", layout="centered", menu_items={"About": "This is a simple Streamlit app for the Interview Agent."})
+
+theme_utils.apply_theme(st.session_state.get('theme', 'Light'))
+
 st.title("Welcome to the Interview Agent App")
 st.write("This is a simple Streamlit app for the Interview Agent.")
+
 
 # --- IGNORE ---
 uploaded_file = st.file_uploader("Upload your resume"+ ":red[*]", type=["pdf", "docx"], help="This field is required")
 job_description = st.text_input("Enter your job description" + ":red[*]", help="This field is required")
 instructions =st.text_input("Enter your Instructions", key="instructions", help="This field is optional")
 
-is_clicked = st.button("Start!", key="start_button")
+if uploaded_file and job_description:
+    IS_DISABLED = False
+is_clicked = st.button(
+    "Start!",
+    key="start_button",
+    disabled=IS_DISABLED,
+    help="Click to start processing",
+    use_container_width=True,
+    on_click=resume_parser
+   )
 
-if is_clicked:
-    if not uploaded_file:
-        st.error("Please upload your resume file.")
-        st.stop()
-    if not job_description:
-        st.error("Please enter the job description.")
-        st.stop()
-    # st.write("Processing your inputs...")
-    with st.spinner("Parsing resume..."):
 
-        resume_parser = barepa.ResumeParser()
-        path = save_uploaded_to_temp(uploaded_file)
-        file_content = resume_parser.parse_resume(path)
-        st.write(file_content)
-            
-            # Here you would add the logic to process the uploaded file and job description
-            # For example, you might call a function like:
-            # response = process_inputs(st.uploaded_file, job_description, st.session_state.instructions)
-        # st.write(response)
-    st.success("Questions generated successfully!")
+    
+
+
